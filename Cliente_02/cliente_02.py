2c0748c12d85396add69d59cc8c91cecacfeab28
Ana Paula <anapaula.p.leonidas@gmail.com>
2025-10-26 19:25:00 -0300

    testes adicionados

---
+++ Cliente_02/cliente_02.py
@@ -0,0 +1,77 @@
+import hashlib
+import random
+import socket
+import statistics
+import time
+from urllib.parse import quote
+
+# constantes
+MATRICULA = "20239037921"
+NOME = "Ana"
+
+lista_tempos = []
+lista_original = ["Scooby Doo na ilha dos zumbis", "Noivas em guerra", "Vestida para casar", "La la land", "Crepúsculo", "Batman", "Aves de Rapina", "Os suspeitos", "Scooby Doo e a bruxa fantasma", "A viagem de chihiro", "Ponyo", "Scooby Doo e o rei dos duendes", "Barbie fairytopia", "Barbie mermaidia", "Barbie a magia do arco iris", "A bussola de ouro", "Coração de tinta", "O labirinto do fauno", "Alice no pais das maravilhas", "Mulherzinhas", "Tinker Bell uma aventura no mundo das fadas"]
+
+random.seed(42)
+lista_filmes = random.choices(lista_original, k=500)
+
+# Endereço IP e porta do servidor
+SERVER_HOST = '79.21.0.4'
+SERVER_PORT = 80
+
+time.sleep(5)
+
+def gerar_ID(matricula, nome):
+    texto = f"{matricula} {nome}"
+    hash_md5 = hashlib.md5(texto.encode('utf-8')).hexdigest()
+    return hash_md5
+
+def gerar_http_requisicao(id, filme):
+    filme_codificado = quote(filme)
+    http_requisicao = f"GET /{id}/{filme_codificado} HTTP/1.1\r\nHost: {SERVER_HOST}\r\n\r\n"
+    return http_requisicao
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+        print(f"Conectando ao servidor em {SERVER_HOST}:{SERVER_PORT}...")
+        s.connect((SERVER_HOST, SERVER_PORT))
+        print("Conectado! Enviando requisição HTTP...")
+        for i in range(10):
+            tempo_inicial = time.perf_counter()
+            for filme in lista_filmes:
+                    X_Custom_ID = gerar_ID(MATRICULA, NOME)
+                    http_requisicao = gerar_http_requisicao(X_Custom_ID, filme)
+                    s.sendall(http_requisicao.encode('utf-8'))
+
+                    response = s.recv(1024)        
+                    print("\n--- Resposta do Servidor ---")
+                    print(response.decode('utf-8'))
+                    print("----------------------------")
+                    #time.sleep(10)
+            tempo_final = time.perf_counter()
+            tempo_rodada = tempo_final - tempo_inicial
+            avg_time = tempo_rodada / 1000
+            lista_tempos.append(tempo_rodada)
+            
+            print(f"\n--- Resultados ---")
+            print(f"Tempo médio por requisição: {avg_time:.4f} segundos")
+
+    except ConnectionRefusedError:
+        print("Erro: A conexão foi recusada. O servidor está rodando?")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+media = statistics.mean(lista_tempos)
+desvio_padrao = statistics.stdev(lista_tempos)
+variancia = statistics.variance(lista_tempos)
+tempo_total = sum(lista_tempos)
+
+print("\n--- Resultados Finais ---")
+print("\nTempos de cada rodada (em segundos):")
+for k, tempo in enumerate(lista_tempos):
+    print(f"  Rodada {k+1}: {tempo:.4f} s")
+print("\nEstatísticas:")
+print(f"  Tempo Total: {tempo_total:.4f} s")
+print(f"  Média: {media:.4f} s")
+print(f"  Variância: {variancia:.4f} s²")
+print(f"  Desvio Padrão: {desvio_padrao:.4f} s")
---
+++ Servidor_concorrente/servidor_concorrente.py
@@ -0,0 +1,88 @@
+import json
+import socket
+import threading
+from urllib.parse import unquote
+
+#constante
+ID = "c8345b76db27676e16c2fe213aec1079"
+
+arquivo = "filmes.json"
+lista_filmes = []
+class Filme:
+    def __init__(self, nome, ano, duracao, sinopse):
+        self.nome = nome
+        self.ano = int(ano)
+        self.duracao = duracao
+        self.sinopse = sinopse
+
+def filmes_json(arquivo):
+    lista = []
+    with open(arquivo, 'r', encoding='utf-8') as f:
+        dados_json = json.load(f)
+        for nome_filme, informacao in dados_json.items():
+            novo_filme = Filme(nome=nome_filme, ano=informacao['ano'], duracao=informacao['duracao'], sinopse=informacao['sinopse'])
+            lista.append(novo_filme)
+    return lista
+
+lista_filmes = filmes_json(arquivo)
+def thread(conn, addr):
+    with conn:
+            print(f"Conexão recebida de {addr}")
+            while True:
+                try:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    metodo, id, filme = extrair_valores_split(data.decode('utf-8'))
+                    http_resposta = ""
+                    if id != ID:
+                        print(f"ID Incorreto: {id}")
+                        http_resposta = "HTTP/1.1 403 Forbidden\n\nID Invalido."
+                    else:
+                        filme_encontrado = None
+                        for i in lista_filmes:
+                            if i.nome == filme:
+                                filme_encontrado = i
+                                break
+                        if filme_encontrado:
+                            print(f"Filme encontrado: {filme_encontrado.nome}")
+                            http_resposta = (f"HTTP/1.1 200 OK\nContent-Type: text/plain; charset=utf-8\n\nAno:{filme_encontrado.ano}\nDuração:{filme_encontrado.duracao}\nSinopse:{filme_encontrado.sinopse}! A resposta veio do servidor.")
+                        else:
+                            print(f"Filme nao encontrado: {filme}")
+                            http_resposta = "HTTP/1.1 404 Not Found\n\nFilme nao encontrado."
+
+                    conn.sendall(http_resposta.encode('utf-8'))
+                except socket.error as e:
+                    print(f"Erro na conexão com {addr}: {e}")
+                    break 
+
+
+def extrair_valores_split(request_string):
+    try:
+        # 1. Pega a primeira linha
+        primeira_linha = request_string.split('\r\n')[0]
+        # 2. Divide a primeira linha em partes (método, path, versão http)
+        partes = primeira_linha.split(' ')
+        metodo = partes[0]
+        path = partes[1] # Será algo como "/123/maca"
+        # 3. Divide o path pelas barras
+        partes_path = path.split('/') # Será algo como ['', '123', 'maca']
+        id_valor = partes_path[1]
+        filme_codificado = partes_path[2]
+        filme_valor = unquote(filme_codificado)
+        return metodo, id_valor, filme_valor
+    except (IndexError, AttributeError):
+        # Falha se a string não tiver o formato esperado
+        return None, None, None
+
+HOST = '79.21.0.4'
+PORT = 80
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Servidor escutando em http://{HOST}:{PORT}")
+    while True:
+        conn, addr = s.accept()
+        cliente_thread = threading.Thread(target=thread, args=(conn, addr))
+        cliente_thread.start()
